6bd80fd3f89dc3b805e4cc9e1037a897ef535e45
orug8m <licht7xinoza1@gmail.com>
2021-10-15 00:56:33 +0900

    step3

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,25 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
 }
 
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
 
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
 // import 'package:flutter/material.dart';
 
 // void main() {
